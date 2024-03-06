5f690338234729fb0ef0b447d718218f516288dd
frexdel <frexdel@gmail.com>
2024-03-06 18:11:23 +0100

    add Module SpringBoot web, jpa, h2, cloud config client, cloud discovry, Oauth2

---
+++ transportConnect/src/main/java/com/frexdel/transportconnect/TransportConnectApplication.java
@@ -0,0 +1,13 @@
+package com.frexdel.transportconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransportConnectApplication.class, args);
+    }
+
+}
---
+++ transportConnect/src/test/java/com/frexdel/transportconnect/TransportConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frexdel.transportconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
